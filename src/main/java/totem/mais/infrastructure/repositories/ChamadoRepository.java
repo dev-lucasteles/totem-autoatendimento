e1a46a218a93ebbb4f0d2bbe7fff952ac6bbac18
Lucas Teles <lucasteles2612@gmail.com>
2026-07-17 11:20:33 -0300

    Add files via upload

--- src/main/java/totem/mais/infrastructure/repositories/ChamadoRepository.java
+++ src/main/java/totem/mais/infrastructure/repositories/ChamadoRepository.java
@@ -7,4 +7,5 @@ import totem.mais.infrastructure.entities.Chamado;
 
 @Repository
 public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
+    // O Spring Boot cria todos os comandos de banco de dados automaticamente aqui
 }
