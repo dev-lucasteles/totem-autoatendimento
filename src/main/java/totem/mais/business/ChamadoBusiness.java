c75cb7da0f8160cd251639689c0b452de54c3ecb
Lucas Teles <lucasteles2612@gmail.com>
2026-07-17 11:18:18 -0300

    Add files via upload

--- src/main/java/totem/mais/business/ChamadoBusiness.java
+++ src/main/java/totem/mais/business/ChamadoBusiness.java
@@ -3,6 +3,7 @@ package totem.mais.business;
 import org.springframework.stereotype.Service;
 import totem.mais.infrastructure.entities.Chamado;
 import totem.mais.infrastructure.repositories.ChamadoRepository;
+import java.util.List;
 
 @Service
 public class ChamadoBusiness {
@@ -13,7 +14,11 @@ public class ChamadoBusiness {
         this.repository = repository;
     }
 
-    //retorna o chamado completo com o ID gerado
+    public List<Chamado> buscarChamadosAbertos() {
+        return repository.findAll();
+    }
+
+    // Agora o método retorna o Chamado completo (com o ID gerado)
     public Chamado processarNovoChamado(String nome, String setor, String problema) {
         if (nome == null || nome.trim().isEmpty()) {
             throw new IllegalArgumentException("ERRO: O nome do funcionário é obrigatório.");
@@ -26,7 +31,6 @@ public class ChamadoBusiness {
         }
 
         Chamado novoChamado = new Chamado(nome, setor, problema);
-        
         return repository.save(novoChamado);
     }
 }
